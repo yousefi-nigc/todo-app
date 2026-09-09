fe1df606c483cc4aabf74755ce2c131d95f42476
شرکت ملی گاز ایران <usefi.nigc@gmail.com>
2026-09-09 14:54:47 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,55 @@
-const CACHE = 'todo-v1';
-const ASSETS = [
-  '/todo-app/',
-  '/todo-app/index.html',
-  '/todo-app/manifest.json',
-  '/todo-app/icon-192.png',
-  '/todo-app/icon-512.png'
+var CACHE_NAME = "todo-v3";
+var ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(ASSETS);
+    }).then(function () {
+      return self.skipWaiting();
+    })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.map(function (key) {
+          if (key !== CACHE_NAME) {
+            return caches["delete"](key);
+          }
+        })
+      );
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      if (cached) {
+        return cached;
+      }
+      return fetch(event.request).then(function (response) {
+        if (event.request.method === "GET" && response && response.status === 200) {
+          var copy = response.clone();
+          caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(event.request, copy);
+          });
+        }
+        return response;
+      }).catch(function () {
+        return caches.match("./index.html");
+      });
+    })
   );
 });
