606787a33dbfbb03a6966648c3d7a432c802d4be
شرکت ملی گاز ایران <usefi.nigc@gmail.com>
2026-09-09 13:16:53 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE = 'todo-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/todo-app/',
+  '/todo-app/index.html',
+  '/todo-app/manifest.json',
+  '/todo-app/icon-192.png',
+  '/todo-app/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
@@ -13,19 +13,14 @@ self.addEventListener('install', e => {
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, copy));
-      return res;
-    }).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
